516a39de2e7c1e491d81eada1d23ea4b29ac2135
Nicolas Torion <nicolas.torion@bearstudio.fr>
2025-02-05 13:50:22 +0100

    Update app

--- app/_layout.js
+++ app/_layout.js
@@ -3,7 +3,24 @@ import { SafeAreaBox, ThemeProvider } from "react-native-ficus-ui";
 
 export default function HomeLayout() {
   return (
-    <ThemeProvider>
+    <ThemeProvider
+      theme={{
+        colors: {
+          brand: {
+            50: "#f8ffde",
+            100: "#edfbb2",
+            200: "#e3f985",
+            300: "#d8f655",
+            400: "#cdf328",
+            500: "#b3da12",
+            600: "#8caa09",
+            700: "#647903",
+            800: "#3b4900",
+            900: "#131900",
+          },
+        },
+      }}
+    >
       <SafeAreaBox flex={1}>
         <Stack
           screenOptions={{
