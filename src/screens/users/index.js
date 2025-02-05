d7e52ecb853d602dbfb365cd8cd733a7cd765ae0
Nicolas Torion <nicolas.torion@bearstudio.fr>
2025-02-05 14:27:50 +0100

    Update app

--- src/screens/users/index.js
+++ src/screens/users/index.js
@@ -9,7 +9,7 @@ import {
 import { useUsers } from "../../hooks/users";
 import { useRouter } from "expo-router";
 import User from "../user";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 const UsersListing = () => {
   const { users, isLoading, refresh } = useUsers();
@@ -22,16 +22,48 @@ const UsersListing = () => {
     maxWidth: 480,
   });
 
-  const handleOpenUserDetails = (user) => () => {
-    if (isSmallScreen) {
-      router.push({
-        pathname: "/user",
-        params: { user: JSON.stringify(user) },
-      });
-    } else {
-      setCurrentUser(JSON.stringify(user));
-    }
-  };
+  const handleOpenUserDetails = useCallback(
+    (user) => () => {
+      if (isSmallScreen) {
+        router.push({
+          pathname: "/user",
+          params: { user: JSON.stringify(user) },
+        });
+      } else {
+        setCurrentUser(JSON.stringify(user));
+      }
+    },
+    [isSmallScreen, router]
+  );
+
+  const renderItem = useCallback(
+    ({ item: user }) => (
+      <TouchableOpacity onPress={handleOpenUserDetails(user)}>
+        <Box
+          flexDirection="row"
+          alignItems="center"
+          bg="gray.100"
+          p="sm"
+          borderRadius={30}
+        >
+          <Image
+            source={{ uri: user?.picture?.medium }}
+            w={50}
+            h={50}
+            borderRadius={100}
+          />
+          <Box ml="lg">
+            <Text
+              fontSize="lg"
+              fontWeight="700"
+            >{`${user.name?.first} ${user.name?.last}`}</Text>
+            <Text>{user.email}</Text>
+          </Box>
+        </Box>
+      </TouchableOpacity>
+    ),
+    [handleOpenUserDetails]
+  );
 
   return (
     <Box h="100%" p="xl" flexDirection="row" justify="space-between">
@@ -46,31 +78,7 @@ const UsersListing = () => {
           onRefresh={refresh}
           data={users}
           keyExtractor={(item) => item?.email}
-          renderItem={({ item: user }) => (
-            <TouchableOpacity onPress={handleOpenUserDetails(user)}>
-              <Box
-                flexDirection="row"
-                alignItems="center"
-                bg="gray.100"
-                p="sm"
-                borderRadius={30}
-              >
-                <Image
-                  source={{ uri: user?.picture?.medium }}
-                  w={50}
-                  h={50}
-                  borderRadius={100}
-                />
-                <Box ml="lg">
-                  <Text
-                    fontSize="lg"
-                    fontWeight="700"
-                  >{`${user.name?.first} ${user.name?.last}`}</Text>
-                  <Text>{user.email}</Text>
-                </Box>
-              </Box>
-            </TouchableOpacity>
-          )}
+          renderItem={renderItem}
         />
       </Box>
 
